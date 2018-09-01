649c309a93418114a87f836a273ecd2917cdad3f
jpascale <jpascale@itba.edu.ar>
2018-09-01 17:25:23 -0300

    Add table creation method

--- storage/src/thulium/storage/postgres_storage.js
+++ storage/src/thulium/storage/postgres_storage.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
 
+
 class PostgresStorage {
 
   constructor(config) {
-    const pool = new Pool({
+    this.pool = new Pool({
       user: 'jpascale',
       host: 'localhost',
       database: 'test',
@@ -17,5 +18,33 @@ class PostgresStorage {
     })
   }
 
+  dropTable(tableName, callback) {
+    const queryStr = `DROP TABLE $1;`
+    if (callback) {
+      this.pool.query(queryStr, [tableName], callback);
+    } else {
+      return this.pool.query(queryStr, [tableName]);
+    }
+  }
+
+  //TODO: check types: int, smallint, real, double precision, char(N), varchar(N), date, time, timestamp, and interval
+  createTable(tableName, fields, callback) {
+    const paramStr = Object.keys(fields).map((_, index) => `$${2 * index + 1} $${2 * index + 2}`).join(', ')
+    const queryStr = `CREATE TABLE ${tableName} (${paramStr});`;
+    const args = Object.keys(fields).reduce((prev, curr) => {
+      prev.push(curr);
+      prev.push(fields[curr]);
+      return prev
+    }, []);
+
+    if (callback) {
+      this.pool.query(queryStr, args, callback);
+    } else {
+      return this.pool.query(queryStr, args);
+    }
+
+  }
+
 }
+
 module.exports = PostgresStorage;
