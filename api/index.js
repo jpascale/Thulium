cede93be3aa61802311b3fcd4abe3e81bd51ec67
Martin Goffan <martin.goffan@me.com>
2018-11-04 16:50:44 -0300

    Server now boots

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const { PostgresStorage } = require('@thulium/storage')
     , { connect } = require("@thulium/internal")
-    , { config } = require('@thulium/base')
+    , { Config } = require('@thulium/base')
     , debug = require('debug')('api:boot')
 
 debug('setting up postgres config');
-PostgresStorage.config(config.postgres);
+PostgresStorage.config(Config.postgres);
 
 connect((err) => {
   if (err) {
