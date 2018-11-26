31c4a360878ae3a90d2b05edb7b5ef2261eded9c
jpascale <jpascale@itba.edu.ar>
2018-11-25 23:13:51 -0300

    Add upload dataset backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const { PostgresStorage } = require('@thulium/storage')
-    , { connect } = require("@thulium/internal")
-    , { Config } = require('@thulium/base')
-    , debug = require('debug')('api:boot')
-    , async = require('async')
-    , bootstrapModules = require('./bootstrap');
+  , { connect } = require("@thulium/internal")
+  , { Config } = require('@thulium/base')
+  , debug = require('debug')('api:boot')
+  , async = require('async')
+  , bootstrapModules = require('./bootstrap');
 
 debug('setting up postgres config');
 PostgresStorage.config(Config.storage.postgres);
