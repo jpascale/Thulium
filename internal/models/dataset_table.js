147ac5a551f5a5ad29d1f80fe13d89eb6edf420b
jpascale <jpascale@itba.edu.ar>
2019-01-15 01:28:46 -0300

    Add dataset & dataset table

---
+++ internal/models/dataset_table.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+  , debug = require('debug')('internal:models:dataset');
+
+debug('configuring dataset schema');
+
+const DatasetTable = mongoose.Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  last_updated: {
+    type: Date,
+    default: Date.now
+  },
+  dataset: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Dataset',
+    index: true
+  },
+  table_name: {
+    type: String,
+  },
+  rows: [{
+    name: String,
+    /* int, string, float, boolean */
+    data_type: {
+      type: String,
+      enum: ['int', 'string', 'float', 'boolean']
+    },
+    options: {}
+  }],
+  values: [mongoose.Schema.Types.Mixed]
+});
+
+module.exports = DatasetTable;
