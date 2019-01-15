c8109e6189ef81c83af2328b85c722fd76abbc73
jpascale <jpascale@itba.edu.ar>
2019-01-15 02:20:53 -0300

    Add dataset instance schema

---
+++ internal/models/dataset_instance.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+  , debug = require('debug')('internal:models:dataset_instance');
+
+debug('configuring dataset_instance schema');
+
+const DatasetInstance = mongoose.Schema({
+  dataset: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Dataset',
+    index: true
+  },
+  /**
+   * Used to check permission on tables.
+   */
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  engine: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Engine'
+  },
+  /**
+   * Key: Name of the table.
+   * Value: Name of the table as it is found in the database.
+   */
+  tables: {
+    type: Map,
+    of: String
+  }
+});
+
+module.exports = Dataset;
