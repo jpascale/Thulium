930ecad2d5e076f229b5177c27f72ac09658dd6f
jpascale <jpascale@itba.edu.ar>
2019-02-06 00:48:51 -0300

    Expose DatasetTable from internal

--- internal/controllers/index.js
+++ internal/controllers/index.js
@@ -2,6 +2,7 @@ module.exports = {
 	User: require('./user'),
 	Session: require('./session'),
 	Dataset: require('./dataset'),
+	DatasetTable: require('./dataset_table'),
 	Engine: require('./engine'),
 	File: require('./file'),
 };
