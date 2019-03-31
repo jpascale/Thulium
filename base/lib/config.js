4e34735e2fc80b04c3b5114f7e90b82736a986bc
Martin Goffan <mgoffan@itba.edu.ar>
2019-03-31 16:46:45 -0300

    Use defaults deep for merging configs

--- base/lib/config.js
+++ base/lib/config.js
@@ -2,14 +2,12 @@ const fs = require('fs')
 		, path = require('path')
 		, configPath = process.env.CONFIG_PATH || path.join(process.cwd(), '..', 'config')
 		, filename = process.env.CONFIG || 'app.json'
-		, credentialFilename = process.env.SECURE_CONFIG || 'app.secure.json';
+		, credentialFilename = process.env.SECURE_CONFIG || 'app.secure.json'
+		, defaultsDeep = require('lodash.defaultsdeep');
 
 const file = JSON.parse(fs.readFileSync(path.resolve(configPath, filename), 'utf8'));
 const credential = JSON.parse(fs.readFileSync(path.resolve(configPath, credentialFilename), 'utf8'));
 const environment = process.env.NODE_ENV || 'development';
-const config = {
-	...file[environment],
-	...credential[environment]
-};
+const config = defaultsDeep({}, file[environment], credential[environment]);
 
 module.exports = config;
