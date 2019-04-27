11d3569b55a64efd581915a53ebd6310451f64ae
jpascale <jpascale@itba.edu.ar>
2019-04-27 21:13:24 +0200

    Export dataset instance

--- internal/controllers/index.js
+++ internal/controllers/index.js
@@ -3,6 +3,7 @@ module.exports = {
 	Session: require('./session'),
 	Dataset: require('./dataset'),
 	DatasetItem: require('./dataset-item'),
+	DatasetInstance: require('./dataset-instance'),
 	Engine: require('./engine'),
 	File: require('./file'),
 	Exam: require('./exam'),
