08489aa0a86750004908ae9b14a219986c216e45
Martin Goffan <mgoffan@itba.edu.ar>
2019-06-18 17:13:07 -0300

    Placed all permissions in UI

--- webapp/src/components/Datasets/CreateDatasetModal/Stages/ReviewActions.js
+++ webapp/src/components/Datasets/CreateDatasetModal/Stages/ReviewActions.js
@@ -1,62 +1,137 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import { connect } from 'react-redux';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormText, Col } from 'reactstrap';
 import classNames from 'classnames';
 import { updateDatasetActions } from '../../../../actions/datasets';
 
-const toLabelText = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.replace(/_/, ' ').slice(1);
-}
+const actionsRepo = {
+	ddl: {
+		can_create: {
+			title: 'CREATE',
+			detail: 'is used to create the database or its objects (like table, index, function, views, store procedure and triggers).'
+		},
+		can_drop: {
+			title: 'DROP',
+			detail: 'is used to delete objects from the database.'
+		},
+		can_alter: {
+			title: 'ALTER',
+			detail: 'is used to alter the structure of the database.'
+		},
+		can_truncate: {
+			title: 'TRUNCATE',
+			detail: 'is used to remove all records from a table, including all spaces allocated for the records are removed.'
+		},
+		can_comment: {
+			title: 'COMMENT',
+			detail: 'is used to add comments to the data dictionary.'
+		},
+		can_rename: {
+			title: 'RENAME',
+			detail: 'is used to rename an object existing in the database.'
+		},
+	},
+
+	dml: {
+		can_insert: {
+			title: 'INSERT',
+			detail: 'is used to insert data into a table.'
+		},
+		can_update: {
+			title: 'UPDATE',
+			detail: 'is used to update existing data within a table.'
+		},
+		can_delete: {
+			title: 'DELETE',
+			detail: 'is used to delete records from a database table.'
+		},
+	},
+
+	dcl: {
+		can_grant: {
+			title: 'GRANT',
+			detail: 'gives user’s access privileges to database.'
+		},
+		can_revoke: {
+			title: 'REVOKE',
+			detail: 'withdraw user’s access privileges given by using the GRANT command.'
+		},
+	},
+
+	tcl: {
+		can_commit: {
+			title: 'COMMIT',
+			detail: 'commits a Transaction.'
+		},
+		can_rollback: {
+			title: 'ROLLBACK',
+			detail: 'rollbacks a transaction in case of any error occurs.'
+		},
+		can_savepoint: {
+			title: 'SAVEPOINT',
+			detail: 'sets a savepoint within a transaction.'
+		},
+		can_set_transaction: {
+			title: 'SET TRANSACTION',
+			detail: 'specify characteristics for the transaction.'
+		},
+	}
+};
+
+const categoryTitles = {
+	ddl: 'DDL (Data Definition Language)',
+	dml: 'DML (Data Manipulation Language)',
+	dcl: 'DCL (Data Control Language)',
+	tcl: 'TCL (transaction Control Language)'
+};
 
 class ReviewActions extends React.Component {
-  state = {
-    actions: {
-      can_create: false,
-      can_alter: false
-    }
-  }
+	state = {
+		actions: Object.keys(actionsRepo).reduce((memo, val) => {
+			Object.assign(memo, Object.keys(actionsRepo[val]).reduce((memo, val) => {
+				memo[val] = false;
+				return memo;
+			}, {}));
+			return memo;
+		}, {})
+	}
 
-  componentDidMount = () => {
-    this.props.updateDatasetActions(this.state.actions);
-  }
+	componentDidMount = () => this.props.updateDatasetActions(this.state.actions)
+	componentDidUpdate = () => this.props.updateDatasetActions(this.state.actions)
 
-  componentDidUpdate = () => {
-    this.props.updateDatasetActions(this.state.actions);
-  }
+	toggleUpdateAction = key => () => {
+		const { actions } = this.state;
+		const nextActions = Object.assign({}, actions, { [key]: !actions[key] });
+		console.log(nextActions);
+		this.setState({ actions: nextActions });
+	}
 
-  toggleUpdateAction = (key) => {
-    this.setState((prevState) => {
-      return {
-        actions: {
-          ...prevState.actions,
-          [key]: !prevState.actions[key]
-        }
-      }
-    });
-  }
-
-  render = () => {
-    const { actions } = this.state;
-    const availableActions = Object.keys(actions);
-    return (
-      <div className="review-datasets d-flex flex-row">
-        <Form>
-          {availableActions.map((action) => {
-            return (
-              <FormGroup key={`formgroup_${action}`} check>
-                <Label key={`label_${action}`} for={action} check>
-                  <Input id={action} key={`input_${action}`} type="checkbox" onChange={() => this.toggleUpdateAction(action)} value={actions[action]} />
-                  {toLabelText(action)}
-                </Label>
-              </FormGroup>
-            )
-          })}
-        </Form>
-      </div>
-    );
-  }
+	render = () => {
+		const { actions } = this.state;
+		return (
+			<div className="review-datasets d-flex flex-column">
+				<h4>Dataset Permissions</h4>
+				<p className="text-muted">Select what is allowed for this dataset</p>
+				<div className="d-flex flex-row">
+					{Object.keys(actionsRepo).map(key => (
+						<Col sm={3}>
+							<h6>{categoryTitles[key]}</h6>
+							<Form>
+								{Object.keys(actionsRepo[key]).map(action => (
+									<FormGroup key={action} check>
+										<Input type="checkbox" onChange={this.toggleUpdateAction(action)} value={actions[action]} />
+										<Label for={action} check>{actionsRepo[key][action].title}</Label>
+										<FormText color="muted">{actionsRepo[key][action].detail}</FormText>
+									</FormGroup>
+								))}
+							</Form>
+						</Col>
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
@@ -64,7 +139,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  updateDatasetActions: (actions) => dispatch(updateDatasetActions(actions))
+	updateDatasetActions: actions => dispatch(updateDatasetActions(actions))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReviewActions);
